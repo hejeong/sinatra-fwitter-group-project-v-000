43fe8e45f39cfca50146d59c1cb984d0daf58e6d
Jonathan Hong <jonhong777@gmail.com>
2018-08-22 22:35:27 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180822142134) do
-
-  create_table "tweets", force: :cascade do |t|
-    t.string  "content"
-    t.integer "user_id"
-  end
+ActiveRecord::Schema.define(version: 20180822142126) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password_digest"
+    t.string "password"
   end
 
 end
